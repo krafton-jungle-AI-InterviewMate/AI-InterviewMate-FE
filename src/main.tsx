002cd386f201d21044fc2cdba7370cf6f82b96b0
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-02-05 22:58:15 +0900

    chore: 린트 설정 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from 'App';
-import GlobalStyles from 'styles/GlobalStyles';
-import RecoilProvider from 'components/providers/RecoilProvider';
-import QueryProvider from 'components/providers/QueryProvider';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "App";
+import GlobalStyles from "styles/GlobalStyles";
+import RecoilProvider from "components/providers/RecoilProvider";
+import QueryProvider from "components/providers/QueryProvider";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryProvider>
       <RecoilProvider>
@@ -13,5 +13,5 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <App />
       </RecoilProvider>
     </QueryProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
