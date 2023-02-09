39e0292649806f8c552fd5bd4917824f6a1e85fd
KKamjangE <ajm980718@gmail.com>
2023-02-10 01:48:41 +0900

    feat: 로비 페이지 생성 및 렌더링 페이지와 연결

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Nav from "components/layout/Nav";
 import Home from "pages/Home";
 import styled from "@emotion/styled";
+import Lobby from "pages/Lobby";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Lobby" element={<Lobby />} />
       </Routes>
     </StyledWrapper>
   );
