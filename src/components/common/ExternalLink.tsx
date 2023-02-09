657cb0a238ae1762bdf729b9fde2e83f16f69592
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-02-09 15:41:08 +0900

    feat: 외부 링크 걸 때 사용할 링크 컴포넌트 추가

---
+++ src/components/common/ExternalLink.tsx
@@ -0,0 +1,13 @@
+import { AnchorHTMLAttributes } from "react";
+
+const ExternalLink = (props: AnchorHTMLAttributes<HTMLAnchorElement>) => {
+  const { className, href, children } = props;
+
+  return (
+    <a className={className} href={href} target="_blank" rel="noopener noreferrer">
+      {children}
+    </a>
+  );
+};
+
+export default ExternalLink;
