0c46c8520fb1980942efefba155928c3b4c6eaee
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-02-10 20:40:24 +0900

    feat: 면접 대기방 마크업 및 스타일링

---
+++ src/components/interview/InterviewReadyContainer/NameTag.tsx
@@ -0,0 +1,64 @@
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+
+type RoleType = "interviewee" | "interviewer";
+
+type NameTagProps = {
+  role: RoleType;
+  profileName: string;
+};
+
+const NameTag = (props: NameTagProps) => {
+  const {
+    role,
+    profileName,
+  } = props;
+
+  return (
+    <StyledNameTag>
+      <StyledRoleTag role={role}>
+        <span>
+          {role === "interviewee" ? "면접자" : "면접관"}
+        </span>
+      </StyledRoleTag>
+      <StyledProfileName>
+        {profileName}
+      </StyledProfileName>
+    </StyledNameTag>
+  );
+};
+
+export default NameTag;
+
+const StyledNameTag = styled.dl`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const StyledRoleTag = styled.dt<{ role: RoleType }>`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+  width: 70px;
+  height: 24px;
+  border-radius: 5px;
+  margin-right: 12px;
+  font-size: 12px;
+
+  ${({ role }) => role === "interviewee" && css`
+    background-color: var(--main-blue);
+    color: var(--main-white);
+  `}
+  ${({ role }) => role === "interviewer" && css`
+    background-color: var(--push-gray);
+    color: var(--main-black);
+  `}
+`;
+
+const StyledProfileName = styled.dd`
+  color: var(--main-black);
+  font-size: 16px;
+`;
