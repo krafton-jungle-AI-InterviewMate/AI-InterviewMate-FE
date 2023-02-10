3183cef3ba4a0af574f1899b48c3a2765dcd1b2d
KKamjangE <ajm980718@gmail.com>
2023-02-10 16:34:32 +0900

    style: 버튼 스타일 컴포넌트 생성

--- src/styles/RedBtn.tsx
+++ src/styles/RedBtn.tsx
@@ -5,24 +5,25 @@ interface BtnSize {
   height: string;
 }
 
-export const StyledOrangeBtn = styled.div<BtnSize>`
+export const StyledRedBtn = styled.div<BtnSize>`
   display: flex;
   justify-content: center;
   align-items: center;
   width: ${props => props.width};
   height: ${props => props.height};
   margin-top: 60px;
-  color: white;
-  background-color: var(--main-orange);
+  color: var(--main-black);
+  background-color: var(--main-white);
   border-radius: ${props => (props.width === "250px" ? "15px 30px" : "5px 15px")};
   font-size: ${props => (props.width === "250px" ? "24px" : "20px")};
   font-weight: 400;
   text-align: center;
   transition: 0.2s;
   &:hover {
-    background-color: var(--light-orange);
+    background-color: var(--main-alert);
+    color: var(--main-white);
   }
   &:active {
-    background-color: var(--push-orange);
+    background-color: var(--push-alert);
   }
 `;
