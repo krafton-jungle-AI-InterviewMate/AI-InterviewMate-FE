1bc3ff1d526b3b2c726f3dc5b31acf07f9265f84
KKamjangE <ajm980718@gmail.com>
2023-02-11 20:44:57 +0900

    refact: 면접 결과 페이지 리팩토링

--- src/components/layout/Result/InterviewResult.tsx
+++ src/components/layout/Result/InterviewResult.tsx
@@ -5,47 +5,45 @@ interface StyledResultProps {
 }
 
 const StyledResult = styled.div<StyledResultProps>`
-  margin-top: 120px;
-  .interviewCard {
-    width: 834px;
-    height: 130px;
-    display: flex;
-    justify-content: space-between;
-    border-radius: 5px 15px;
-    border: 1px solid var(--main-gray);
-    padding: 24px 32px;
-    box-shadow: 0px 4px 24px rgba(0, 0, 0, 0.04);
-    .nameDate {
-      text-align: left;
-      h2 {
-        font-size: 24px;
-        font-weight: 500;
-        color: var(--main-black);
-        margin: 0 0 14px;
-      }
-      p {
-        font-size: 14px;
-        color: var(--main-black);
-        margin: 0;
-      }
+  width: 834px;
+  height: 130px;
+  display: flex;
+  justify-content: space-between;
+  border-radius: 5px 15px;
+  border: 1px solid var(--main-gray);
+  padding: 24px 32px;
+  margin-bottom: 50px;
+  box-shadow: 0px 4px 24px rgba(0, 0, 0, 0.04);
+  .nameDate {
+    text-align: left;
+    h2 {
+      font-size: 24px;
+      font-weight: 500;
+      color: var(--main-black);
+      margin: 0 0 14px;
     }
-    .infoTime {
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      .interviewer {
-        width: 100px;
-        height: 24px;
-        font-size: 12px;
-        color: var(--main-white);
-        background-color: ${props =>
-          props.isAiInterview ? "var(--push-gray)" : "var(--main-black)"};
-        border-radius: 5px;
-      }
-      span {
-        font-size: 14px;
-        color: var(--font-gray);
-      }
+    p {
+      font-size: 14px;
+      color: var(--main-black);
+      margin: 0;
+    }
+  }
+  .infoTime {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .interviewer {
+      width: 100px;
+      height: 24px;
+      font-size: 12px;
+      color: var(--main-white);
+      background-color: ${props =>
+        props.isAiInterview ? "var(--push-gray)" : "var(--main-black)"};
+      border-radius: 5px;
+    }
+    span {
+      font-size: 14px;
+      color: var(--font-gray);
     }
   }
 `;
@@ -67,19 +65,17 @@ function InterviewResult({
 }: InterviewResultProps) {
   return (
     <StyledResult isAiInterview={true}>
-      <div className="interviewCard">
-        <div className="nameDate">
-          <h2>{roomName}</h2>
-          <p>{interviewDate}</p>
-        </div>
-        <div className="infoTime">
-          <div className="interviewer">{isAiInterview ? "AI 면접관" : "유저 면접관"}</div>
-          {isAiInterview ? (
-            <span className="interviewTime">질문 개수: {question}개</span>
-          ) : (
-            <span className="interviewTime">진행 시간: {interviewTime}분</span>
-          )}
-        </div>
+      <div className="nameDate">
+        <h2>{roomName}</h2>
+        <p>{interviewDate}</p>
+      </div>
+      <div className="infoTime">
+        <div className="interviewer">{isAiInterview ? "AI 면접관" : "유저 면접관"}</div>
+        {isAiInterview ? (
+          <span className="interviewTime">질문 개수: {question}개</span>
+        ) : (
+          <span className="interviewTime">진행 시간: {interviewTime}분</span>
+        )}
       </div>
     </StyledResult>
   );
