871219d5482302a9740b20b1838c057388e6c262
KKamjangE <ajm980718@gmail.com>
2023-02-11 20:45:15 +0900

    refact: 면접 결과 페이지 리팩토링

--- src/pages/mypage/Result.tsx
+++ src/pages/mypage/Result.tsx
@@ -1,13 +1,26 @@
 import InterviewResult from "components/layout/Result/InterviewResult";
+import styled from "@emotion/styled";
+
+const StyledResult = styled.div`
+  margin-top: 120px;
+`;
 
 function Result() {
   return (
-    <InterviewResult
-      roomName="JS 면접 준비"
-      isAiInterview={true}
-      interviewDate="2월 11일 토요일 08:30 면접"
-      question={3}
-    />
+    <StyledResult>
+      <InterviewResult
+        roomName="JS 면접 준비"
+        isAiInterview={true}
+        interviewDate="2월 11일 토요일 08:30 면접"
+        question={3}
+      />
+      <InterviewResult
+        roomName="좋은 면접 좋은 자리"
+        isAiInterview={true}
+        interviewDate="2월 11일 토요일 08:43 면접"
+        question={5}
+      />
+    </StyledResult>
   );
 }
 
