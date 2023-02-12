f74157fd4fb687a6731e15738c0c26c9f982e86c
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-02-12 22:17:45 +0900

    feat: 피드백 컴포넌트 구현

---
+++ src/components/interview/InterviewFeedback.tsx
@@ -0,0 +1,57 @@
+import { InterviewFeedbackTypes } from "types/interview";
+import { InterviewFeedbackComment } from "constants/interview";
+
+import { AiOutlineEye } from "react-icons/ai";
+import { GrUserManager } from "react-icons/gr";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+
+type InterviewFeedbackProps = {
+  feedbackType: InterviewFeedbackTypes;
+};
+
+const InterviewFeedback = (props: InterviewFeedbackProps) => {
+  const {
+    feedbackType,
+  } = props;
+
+  return (
+    <StyledWrap role="alert" feedbackType={feedbackType}>
+      {feedbackType === "iris" && <AiOutlineEye />}
+      {feedbackType === "motion" && <GrUserManager />}
+      <p>{InterviewFeedbackComment[feedbackType]}</p>
+    </StyledWrap>
+  );
+};
+
+export default InterviewFeedback;
+
+const StyledWrap = styled.div<InterviewFeedbackProps>`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-evenly;
+  align-items: center;
+  position: absolute;
+  bottom: 60px;
+  transform: translate(-50%, 0);
+  width: 254px;
+  height: 38px;
+  background-color: rgba(24, 24, 24, 0.8);
+  border-radius: var(--button-border-radius);
+  color: var(--main-white);
+
+  ${({ feedbackType }) => feedbackType === "iris" && css`
+    left: calc(50% - 140px);
+  `}
+  ${({ feedbackType }) => feedbackType === "motion" && css`
+    left: calc(50% + 140px);
+  `}
+
+  & p {
+    font-size: 14px;
+  }
+  & path {
+    color: var(--main-white);
+    stroke: var(--main-white);
+  }
+`;
