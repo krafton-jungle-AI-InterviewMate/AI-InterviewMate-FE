6a0da7e299c9de50d54a86213e7569616a107528
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-02-13 02:58:20 +0900

    feat: 면접 결과 상세 페이지 퍼블리싱

--- src/pages/mypage/ResultDetails.tsx
+++ src/pages/mypage/ResultDetails.tsx
@@ -1,5 +1,82 @@
+import ResultTable from "components/mypage/ResultTable";
+
+import styled from "@emotion/styled";
+
 const ResultDetails = () => {
-  return <div>결과 상세 페이지</div>;
+  return (
+    <StyledWrapper>
+      <StyledHeader>
+        <div className="left-section">
+          <div className="left-section__role-tag">
+            <span>AI 면접관</span>
+          </div>
+          <h2>
+            JS 면접 준비
+          </h2>
+        </div>
+        <div className="right-section">
+          <p className="right-section__time">2월 6일 일요일 17:32 면접</p>
+          <p className="right-section__option">질문 개수: 3개</p>
+        </div>
+      </StyledHeader>
+      <ResultTable />
+    </StyledWrapper>
+  );
 };
 
 export default ResultDetails;
+
+const StyledWrapper = styled.section`
+  width: 900px;
+  margin-top: 70px;
+`;
+
+const StyledHeader = styled.header`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: baseline;
+  margin-bottom: 56px;
+
+  .left-section {
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+
+    &__role-tag {
+      width: 100px;
+      height: 24px;
+      background-color: var(--push-gray);
+      border-radius: 5px;
+
+      & span {
+        font-size: 12px;
+        color: var(--main-white);
+      }
+    }
+
+    & h2 {
+      margin: 0;
+      margin-left: 13px;
+      font-weight: 500;
+      font-size: 24px;
+    }
+  }
+
+  .right-section {
+    font-size: 14px;
+    text-align: right;
+
+    & p {
+      margin: 0;
+      padding: 0;
+    }
+
+    &__time {
+      color: var(--main-black);
+    }
+    &__option {
+      color: var(--font-gray);
+    }
+  }
+`;
