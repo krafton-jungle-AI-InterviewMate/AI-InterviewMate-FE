398f1b6eda2e633b16261f857f1b147f20449a5d
KKamjangE <ajm980718@gmail.com>
2023-02-13 23:45:35 +0900

    feat: 500 에러 페이지 구현

--- src/pages/mypage/Result.tsx
+++ src/pages/mypage/Result.tsx
@@ -5,6 +5,7 @@ import { useGetRatingHistory } from "hooks/queries/mypage";
 import { useEffect, useState } from "react";
 import { RatingHistory } from "api/mypage/types";
 import Loading from "pages/Loading";
+import ServerError from "pages/ServerError";
 
 const StyledResult = styled.div`
   margin-top: 120px;
@@ -23,6 +24,8 @@ function Result() {
     <StyledResult>
       {isLoading ? (
         <Loading margin="120px 0 0" />
+      ) : !isError ? (
+        <ServerError />
       ) : (
         historys.map(history => (
           <Link key={history.roomIdx} to={`/mypage/result/${history.roomIdx}`}>
