224d5e54c97116d564315eab853f6fd4378a6154
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-02-14 05:53:24 +0900

    feat: 날짜 포맷 유틸 함수 추가

---
+++ src/lib/format.ts
@@ -0,0 +1,15 @@
+export const formatDate = (dateStr: string) => {
+  const date = new Date(dateStr);
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  };
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    hour: "2-digit",
+    minute: "2-digit",
+  };
+
+  const time = date.toLocaleTimeString("ko-KR", timeOptions);
+  return `${date.toLocaleDateString("ko-KR", dateOptions)} ${time}`;
+};
