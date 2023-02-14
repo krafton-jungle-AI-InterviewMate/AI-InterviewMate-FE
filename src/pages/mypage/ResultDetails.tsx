c536c3095962d8a33f68ea46408e195f0c7438bc
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-02-14 18:56:58 +0900

    feat: 면접 결과 상세 페이지 API 연동 (WIP)

--- src/pages/mypage/ResultDetails.tsx
+++ src/pages/mypage/ResultDetails.tsx
@@ -1,25 +1,57 @@
+import { useSearchParams } from "react-router-dom";
+import { useGetRatingDetail } from "hooks/queries/mypage";
+import { RoomTypes } from "api/mypage/types";
+
 import ResultTable from "components/mypage/ResultTable";
+import Loading from "components/common/Loading";
 
 import styled from "@emotion/styled";
 
 const ResultDetails = () => {
+  const [ searchParams ] = useSearchParams();
+
+  const {
+    data,
+    isFetching,
+    isError,
+  } = useGetRatingDetail(
+    Number(searchParams.get("room")),
+    searchParams.get("type") as RoomTypes,
+  );
+
+  if (isFetching) {
+    return (
+      <StyledWrapper>
+        <Loading margin="0" />
+      </StyledWrapper>
+    );
+  }
+
   return (
     <StyledWrapper>
-      <StyledHeader>
-        <div className="left-section">
-          <div className="left-section__role-tag">
-            <span>AI 면접관</span>
-          </div>
-          <h2>
-            JS 면접 준비
-          </h2>
+      {isError ? (
+        <div>
+          <p>데이터를 불러오는 중 에러가 발생했습니다.</p>
         </div>
-        <div className="right-section">
-          <p className="right-section__time">2월 6일 일요일 17:32 면접</p>
-          <p className="right-section__option">질문 개수: 3개</p>
-        </div>
-      </StyledHeader>
-      <ResultTable />
+      ) : (
+        <>
+          <StyledHeader>
+            <div className="left-section">
+              <div className="left-section__role-tag">
+                <span>AI 면접관</span>
+              </div>
+              <h2>
+                JS 면접 준비
+              </h2>
+            </div>
+            <div className="right-section">
+              <p className="right-section__time">2월 6일 일요일 17:32 면접</p>
+              <p className="right-section__option">질문 개수: 3개</p>
+            </div>
+          </StyledHeader>
+          <ResultTable />
+        </>
+      )}
     </StyledWrapper>
   );
 };
