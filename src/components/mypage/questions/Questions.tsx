608f9a359ec9b64cbde836f67036e0032fd70415
KKamjangE <ajm980718@gmail.com>
2023-02-20 21:27:56 +0900

    feat: 질문 꾸러미 페이지 구현

--- src/components/mypage/questions/Questions.tsx
+++ src/components/mypage/questions/Questions.tsx
@@ -1,6 +1,8 @@
 import styled from "@emotion/styled";
+import { Dialog, DialogActions, DialogTitle } from "@mui/material";
 import { Link } from "react-router-dom";
 import { StyledBtn } from "styles/StyledBtn";
+import { useState } from "react";
 
 const StyledQuestions = styled.div`
   width: 900px;
@@ -26,6 +28,10 @@ const StyledQuestions = styled.div`
         margin-right: 24px;
       }
     }
+    .dialogContents {
+      font-size: 24px;
+      font-weight: 400;
+    }
   }
 `;
 
@@ -47,9 +53,17 @@ const StyledLink = styled(Link)`
   }
 `;
 
+const StyledBtnStlyed = styled(StyledBtn)`
+  margin-right: 25px;
+`;
+
 const Questions = ({ boxName, idx, questionNum }) => {
-  const onClickDelete = () => {
-    console.log("delete");
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClickDelete = () => {
+    setIsOpen(true);
+  };
+  const handleClickClose = () => {
+    setIsOpen(false);
   };
   return (
     <StyledQuestions>
@@ -58,9 +72,39 @@ const Questions = ({ boxName, idx, questionNum }) => {
           <h2>{boxName}</h2>
           <span>{questionNum} / 30</span>
         </StyledLink>
-        <StyledBtn onClick={onClickDelete} width="100px" height="32px" color="red">
+        <StyledBtn onClick={handleClickDelete} width="100px" height="32px" color="red">
           비우기
         </StyledBtn>
+        <Dialog
+          open={isOpen}
+          onClose={handleClickClose}
+          PaperProps={{
+            style: {
+              borderRadius: "10px",
+              display: "flex",
+              alignItems: "center",
+              padding: "50px 35px",
+            },
+          }}
+        >
+          <DialogTitle
+            fontSize={24}
+            fontWeight={400}
+            color={"var(--main-black)"}
+            marginBottom={9}
+            padding={0}
+          >
+            꾸러미를 비우시겠습니까?
+          </DialogTitle>
+          <DialogActions className="dialogActions">
+            <StyledBtnStlyed onClick={handleClickClose} width="200px" height="42px" color="orange">
+              네!
+            </StyledBtnStlyed>
+            <StyledBtn onClick={handleClickClose} width="200px" height="42px" color="red">
+              취소
+            </StyledBtn>
+          </DialogActions>
+        </Dialog>
       </div>
     </StyledQuestions>
   );
--- src/pages/mypage/QuestionList.tsx
+++ src/pages/mypage/QuestionList.tsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { questionBoxes } from "api/questionBoxes/type";
 import Loading from "components/common/Loading";
+import ServerError from "components/common/ServerError";
 
 const StyledQuestionList = styled.div`
   margin-top: 120px;
@@ -18,12 +19,33 @@ const QuestionList = () => {
       setQuestionBoxes(data.data.data);
     }
   }, [isLoading]);
+
+  // 더미 데이터
+  const test = [
+    {
+      idx: 1,
+      boxName: "테스트",
+      questionNum: 3,
+    },
+    {
+      idx: 2,
+      boxName: "테스트2",
+      questionNum: 10,
+    },
+    {
+      idx: 3,
+      boxName: "테스트333",
+      questionNum: 5,
+    },
+  ];
   return (
     <StyledQuestionList>
       {isLoading ? (
         <Loading margin="120" />
+      ) : !isError ? (
+        <ServerError />
       ) : (
-        questionBoxes.map(data => (
+        test.map(data => (
           <Questions
             key={data.idx}
             boxName={data.boxName}
