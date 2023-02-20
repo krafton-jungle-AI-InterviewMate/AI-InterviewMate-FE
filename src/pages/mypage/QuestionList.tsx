f45383a952304c4df6de52bd95ef9ad06fb5cfa8
KKamjangE <ajm980718@gmail.com>
2023-02-20 17:30:51 +0900

    faet: 질문 꾸러미 리스트 GET api 연동

--- src/pages/mypage/QuestionList.tsx
+++ src/pages/mypage/QuestionList.tsx
@@ -1,14 +1,37 @@
 import styled from "@emotion/styled";
 import Questions from "components/mypage/questions/Questions";
+import { useGetQuestionBoxes } from "hooks/queries/questionBoxes";
+import { useEffect } from "react";
+import { useState } from "react";
+import { questionBoxes } from "api/questionBoxes/type";
+import Loading from "components/common/Loading";
 
 const StyledQuestionList = styled.div`
   margin-top: 120px;
 `;
 
 const QuestionList = () => {
+  const [questionBoxes, setQuestionBoxes] = useState<questionBoxes[]>([]);
+  const { data, isLoading, isSuccess, isError } = useGetQuestionBoxes("4");
+  useEffect(() => {
+    if (!isLoading && data) {
+      setQuestionBoxes(data.data.data);
+    }
+  }, [isLoading]);
   return (
     <StyledQuestionList>
-      <Questions />
+      {isLoading ? (
+        <Loading margin="120" />
+      ) : (
+        questionBoxes.map(data => (
+          <Questions
+            key={data.idx}
+            boxName={data.boxName}
+            idx={data.idx}
+            questionNum={data.questionNum}
+          />
+        ))
+      )}
     </StyledQuestionList>
   );
 };
