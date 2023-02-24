7c04068abfac3a85a438ef515e1b51cf4bfc8eba
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-02-24 15:24:46 +0900

    chore: lambda function 재설정(SSM 환경변수 권한 이슈)

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -10,6 +10,15 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolName": "string"
     }
   },
+  "function": {
+    "tts": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
   "storage": {
     "assetStorage": {
       "BucketName": "string",
