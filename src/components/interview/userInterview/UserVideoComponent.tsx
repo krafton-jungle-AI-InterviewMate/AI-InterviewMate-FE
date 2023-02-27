977df725607da550795fd86eaa80e644fe18f92d
KKamjangE <ajm980718@gmail.com>
2023-02-28 00:51:05 +0900

    feat: 유저 면접관 비디오 스타일 및 이름 파싱

--- src/components/interview/userInterview/UserVideoComponent.tsx
+++ src/components/interview/userInterview/UserVideoComponent.tsx
@@ -1,46 +1,58 @@
-import styled from "@emotion/styled";
 import OpenViduVideoComponent from "./OvVideo";
 import { useRecoilValue } from "recoil";
 import { interviewDataAtom } from "store/interview/atom";
+import styled from "@emotion/styled";
 
-const UserVideoComponent = ({ streamManager }) => {
-  const InterviewData = useRecoilValue(interviewDataAtom);
+const UserVideoComponent = ({ streamManager, isInterviewer }) => {
+  const interviewData = useRecoilValue(interviewDataAtom);
   return (
-    <StyledUserVideoComponent>
+    <StyledUserVideoComponent isInterviewer={isInterviewer}>
       {streamManager !== undefined ? (
-        <>
-          <div className="streamcomponent">
-            <OpenViduVideoComponent streamManager={streamManager} />
-          </div>
+        <div className="streamcomponent">
+          <OpenViduVideoComponent streamManager={streamManager} />
           <p>
-            <span className="interviewr">면접자</span>
-            <span>{InterviewData?.nickname}</span>
+            <span className="interviewer">{isInterviewer ? "면접관" : "면접자"}</span>
+            <span className="nickname">{streamManager.stream.connection.data.split('"')[3]}</span>
           </p>
-        </>
-      ) : (
-        <div className="streamcomponent"></div>
-      )}
+        </div>
+      ) : null}
     </StyledUserVideoComponent>
   );
 };
 
-const StyledUserVideoComponent = styled.div`
+interface StyledUserVideoComponentProps {
+  isInterviewer: boolean;
+}
+
+const StyledUserVideoComponent = styled.div<StyledUserVideoComponentProps>`
+  margin-bottom: 20px;
+  .streamcomponent {
+    display: flex;
+    flex-direction: ${props => (props.isInterviewer ? "row" : "column")};
+  }
   p {
     display: flex;
     justify-content: center;
-    font-size: 16px;
+    align-items: center;
+    margin: ${props => (props.isInterviewer ? "0 0 0 50px" : "45px 0 0")};
     span {
       display: block;
     }
-  }
-  .interviewr {
-    width: 70px;
-    height: 24px;
-    border-radius: 5px;
-    margin-right: 12px;
-    font-size: 12px;
-    background-color: var(--main-blue);
-    color: var(--main-white);
+    .interviewer {
+      width: 70px;
+      height: 24px;
+      border-radius: 5px;
+      font-size: 12px;
+      font-weight: 400;
+      margin-right: 12px;
+      color: var(--main-white);
+      background-color: ${props => (props.isInterviewer ? "var(--push-gray)" : "var(--main-blue)")};
+    }
+    .nickname {
+      font-size: 16px;
+      font-weight: 500;
+      color: var(--main-black);
+    }
   }
 `;
 
