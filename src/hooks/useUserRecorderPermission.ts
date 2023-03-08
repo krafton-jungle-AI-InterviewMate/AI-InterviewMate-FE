d0afaef8a2e088b5d945d2bbc40b19bb1efae656
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-03-08 11:47:20 +0900

    feat: 대인면접 음성 합성을 위한 훅 추가(기태님 코드 반영)

---
+++ src/hooks/useUserRecorderPermission.ts
@@ -0,0 +1,61 @@
+import { useEffect } from "react";
+import { useRecoilValue, useRecoilState } from "recoil";
+import {
+  isInterviewerAtom,
+  recordModeAtom,
+  userRecorderAtom,
+} from "store/interview/atom";
+import { MultiStreamRecorder } from "recordrtc";
+import { Subscriber } from "openvidu-browser";
+
+export const useUserRecorderPermission = (subscribers: Array<Subscriber>) => {
+  const isInterviewer = useRecoilValue(isInterviewerAtom);
+  const isRecordMode = useRecoilValue(recordModeAtom);
+  const [ recorder, setRecorder ] = useRecoilState(userRecorderAtom);
+
+  const getPermissionInitializeUserRecorder = async () => {
+    // 저장할 음성 데이터 배열 생성
+    const audioTracks: MediaStreamTrack[] = [];
+
+    // 모든 subscriber에서 오디오 트랙을 추출하여 배열에 추가
+    subscribers.forEach((subscriber) => {
+      const { stream } = subscriber;
+      const audioTrack = stream.getMediaStream().getAudioTracks()[0];
+
+      audioTracks.push(audioTrack);
+    });
+    // 모든 오디오 트랙을 믹싱하여 새 MediaStream 객체 생성
+    const mixedAudioStream = new MediaStream(audioTracks);
+    // 사용자 웹캠 및 마이크 데이터 불러오기 -> 원래 getPermissionInitializeUserRecorder에 있던 코드!!
+    let userStream = await (navigator as any).mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    // Recorder 할 객체 생성
+    const rec = new MultiStreamRecorder([ userStream, mixedAudioStream ], {
+      mimeType: "video/webm",
+    });
+    setRecorder(rec);
+
+    // 영상 녹화
+    rec.record();
+  };
+
+  useEffect(() => {
+    if (isRecordMode && !isInterviewer) {
+      (async () => {
+        await getPermissionInitializeUserRecorder();
+      })();
+  
+      return (() => {
+        if (recorder) {
+          recorder.clearRecordedData();
+        }
+      });
+    }
+  }, []);
+
+  return {
+    getPermissionInitializeUserRecorder,
+  };
+};
