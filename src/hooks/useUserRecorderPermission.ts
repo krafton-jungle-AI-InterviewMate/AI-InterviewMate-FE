f2493012f0037a5a2fd734fd9b6165e282cc014e
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-03-08 14:14:11 +0900

    fix: 면접관 다수일 경우에 대한 음성 합성 이슈 처리(형기님 코드 반영)

--- src/hooks/useUserRecorderPermission.ts
+++ src/hooks/useUserRecorderPermission.ts
@@ -15,28 +15,28 @@ export const useUserRecorderPermission = (subscribers: Array<Subscriber>) => {
 
   const getPermissionInitializeUserRecorder = async () => {
     // 저장할 음성 데이터 배열 생성
-    const audioTracks: MediaStreamTrack[] = [];
-
+    const mediaStreams: MediaStream[] = [];
+  
     // 모든 subscriber에서 오디오 트랙을 추출하여 배열에 추가
-    subscribers.forEach((subscriber) => {
+    subscribers.forEach(subscriber => {
       const { stream } = subscriber;
       const audioTrack = stream.getMediaStream().getAudioTracks()[0];
+      const mixedAudioStream = new MediaStream([ audioTrack ]);
 
-      audioTracks.push(audioTrack);
+      mediaStreams.push(mixedAudioStream);
     });
     // 모든 오디오 트랙을 믹싱하여 새 MediaStream 객체 생성
-    const mixedAudioStream = new MediaStream(audioTracks);
     // 사용자 웹캠 및 마이크 데이터 불러오기 -> 원래 getPermissionInitializeUserRecorder에 있던 코드!!
-    let userStream = await (navigator as any).mediaDevices.getUserMedia({
+    const userStream = await (navigator as any).mediaDevices.getUserMedia({
       video: true,
       audio: true,
     });
     // Recorder 할 객체 생성
-    const rec = new MultiStreamRecorder([ userStream, mixedAudioStream ], {
+    const rec = new MultiStreamRecorder([ userStream, ...mediaStreams ], {
       mimeType: "video/webm",
     });
     setRecorder(rec);
-
+  
     // 영상 녹화
     rec.record();
   };
